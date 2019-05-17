632e67a2835e839b333b414d16a8d2bd1e536e27
SonalY7 <sonal10yadav@gmail.com>
2019-05-17 11:33:19 +0530

    added delete and update feature

--- app.js
+++ app.js
@@ -4,26 +4,55 @@ const port = 8000;
 const bodyParser = require("body-parser");
 const todo = require("./sequelize");
 
-app.set("view engine", "ejs");
-app.use(bodyParser.urlencoded({extended: true}));
-app.use('/public',express.static('public'));
+// app.set("view engine", "ejs");
+// app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use("/public", express.static("public"));
 
-// fetching the todo-list from db.
-app.get("/", (req, res) => {
-  todo.findAll({raw: true}).then(todos => { 
-    res.render("base.ejs", { todoList: todos});
+// retrieve
+app.get("/todo", (req, res) => {
+  todo.findAll({ raw: true }).then(todos => {
+    res.send(todos);
   });
 });
 
-// adding the entered todo item in the db.
-app.post("/newtoDO", (req, res) => {
-  todo.create({text: req.body.item}).then(() => {
-    console.log(`Todo item added to database.`)
-    res.redirect("/");
+// create
+app.post("/todo", (req, res) => {
+  todo.create({ text: req.body.text }).then(() => {
+    res.redirect("/todo");
   });
+});
+
+//update
+app.put("/todo/:id", (req, res) => {
+  todo
+    .update(
+      {
+        text: req.body.text
+      },
+      {
+        where: { id: req.params.id }
+      }
+    )
+    .then(() => {
+      res.redirect("/todo");
+    });
+});
+
+// delete
+app.delete("/todo/:id", (req, res) => {
+  todo
+    .destroy({
+      where: {
+        id: req.params.id
+      }
+    })
+    .then(() => {
+      res.redirect("/todo");
+    });
 });
 
 // for undefined urls.
-app.get("*", (req, res) => res.send("<h1>Invalid route!</h1>"));
+app.get("*", (req, res) => res.send("Invalid route!"));
 
 app.listen(port, () => console.log(`Todo app listening on port ${port}!`));
